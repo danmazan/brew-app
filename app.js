755ff84aa793a67b9bfd84a546188c2cad036b66
danmazan <dan.mazan3@gmail.com>
2023-01-31 13:03:16 +0100

    finished app flow + refactoring

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const observer = new IntersectionObserver((entires) => {
     entires.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
-            entry.target.classList.add('show');
+            entry.target.classList.add('showAnim');
         } else {
-            entry.target.classList.remove('show');
+            entry.target.classList.remove('showAnim');
         }
     });
 })
 
 
 
-const hiddenElements = document.querySelectorAll('.hidden');
+const hiddenElements = document.querySelectorAll('.hiddenAnim');
 
 hiddenElements.forEach((el) => observer.observe(el));
--- flow.js
+++ flow.js
@@ -1,15 +1,47 @@
-// homeScreen -> menu -> different methods 
+// ATTEMPT FOR ANIMATION
 
-$(".btn-homescreen").click(function () { 
-    $("#homescreen").addClass("hidden");
-    $("#menu").removeClass("hidden");
+// var hideAnim = {
+//     visibility: 'hidden',
+//     opacity: 0,
+//     filter: "blur(5px)",
+//     transform: "translateX(-100%)",
+//     transition: "all 1s"
+// }
+
+// homeScreen -> menu -> different methods 
+$(".btn-homescreen").click(function () {
+    changeScreen("#homescreen", "#menu");
 });
 
-$(".btn-menu").click(function (e) { 
+$(".btn-menu").click(function () {
     // console.log(e.target.class);
     if ($(this).hasClass("hoffman")) {
-        $("#menu").addClass("hidden");
-        $("#hoffmanPreq").removeClass("hidden");
+        changeScreen("#menu", "#hoffmanPreq");
     }
-    console.log($(this).hasClass("hoffman"));
+    if ($(this).hasClass("kasuya")) {
+        changeScreen("#menu", "#kasuyaPreq");
+    }
+    if ($(this).hasClass("osmotic")) {
+        changeScreen("#menu", "#osmoticPreq")
+    }
 });
+
+$(".btn-next").click(function () {
+    if ($(this).hasClass("hoffman")) {
+        changeScreen("#hoffmanPreq", "#hoffmanDemo");
+    }
+    if ($(this).hasClass("kasuya")) {
+        changeScreen("#kasuyaPreq", "#kasuyaDemo");
+    }
+    if ($(this).hasClass("osmotic")) {
+        changeScreen("#osmoticPreq", "#osmoticDemo");
+    }
+});
+
+function changeScreen(prevPage, nextPage) {
+    // $(prevPage).addClass("hidden");
+    // $(nextPage).removeClass("hidden");
+    // $(nextPage).addClass("showAnim");
+    $(prevPage).hide();
+    $(nextPage).show();
+}
